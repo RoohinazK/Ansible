9140aa04be25ea1e492e3e50f74f5e1d055c5804
RoohinazK <roohinazk25@gmail.com>
2020-05-03 03:44:41 +0000

    Ansible

---
+++ variables.tf
@@ -0,0 +1,15 @@
+variable "key_name" {
+  default = "terraform-keypair"
+}
+
+variable "pvt_key" {
+  default = "/root/.ssh/mkey.pem"
+}
+
+variable "us-east-zones" {
+  default = ["us-east-1a", "us-east-1b"]
+}
+
+variable "sg-id" {
+  default = "sg-0b0a8584a8436e304"
+}
